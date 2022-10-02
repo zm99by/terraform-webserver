f8d41521cdc5b6f8cc24dc8a1c15ffd55a124e8c
Vladimir Sakhonchik <zm99by@gmail.com>
2022-10-02 13:24:34 +0200

    change image boot disk for webserver

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "webserver_ip" {
   value = google_compute_instance.vm.network_interface.0.access_config.0.nat_ip
+  value = google_compute_instance.web.network_interface.0.access_config.0.nat_ip
 }

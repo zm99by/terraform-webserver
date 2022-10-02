e042d7f1d2dc404a9c015aa0a9c7eb2f9ebbbe4d
Vladimir Sakhonchik <zm99by@gmail.com>
2022-10-02 13:27:37 +0200

    add second output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,6 @@
 output "webserver_ip" {
   value = google_compute_instance.vm.network_interface.0.access_config.0.nat_ip
+}
+output "web_ip" {
   value = google_compute_instance.web.network_interface.0.access_config.0.nat_ip
 }
